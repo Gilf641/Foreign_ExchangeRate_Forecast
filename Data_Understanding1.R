a9396fff4dc1ed377a955447995653d04eeaaea1
RohanShetty <shettyrohan641@yahoo.com>
2019-09-30 03:39:45 +0530

    Stuck with returns!

--- Data_Understanding1.R
+++ Data_Understanding1.R
@@ -13,3 +13,19 @@ gbp_data <- readTheFile('USDGBP')
 eur_data <- readTheFile('USDEUR')
 
 
+#Now I need to calculate the returns
+
+
+summary(cny_data)
+#first conv it into float
+#then set r0 = 0
+#then set r1 = p1 - p0
+options(digits = 4)
+returns <- function(sheetname){
+  r = as.double(sheetname[, 1])
+  print(r)
+}
+
+
+cnyr <- returns(cny_data)
+#I'm stuck over here
